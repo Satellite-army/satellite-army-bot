4c40a57773d359b1243da65d857e7946c43fe64d
Marco Carrara <marco.carrara@runelab.it>
2021-06-06 00:06:42 +0200

    fix(express): change port

--- express.js
+++ express.js
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
     res.status(200).json({ status: 'ok' })
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.PORT || port, () => {
+    console.log(`Example app listening at port ${port}`)
 })
